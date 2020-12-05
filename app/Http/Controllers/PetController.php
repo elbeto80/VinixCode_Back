20781005f981a0805c879ec15a5a13ea42b1d533
elbeto80 <elbeto80@hotmail.com>
2020-12-05 16:01:49 -0500

    Se reciben objetos al guardar mascota y se transforman en arreglo para manuipulación. Se guarda la imagen y se valida

--- app/Http/Controllers/PetController.php
+++ app/Http/Controllers/PetController.php
@@ -53,25 +53,47 @@ class PetController extends Controller
     public function savePet(Request $request) {
         try {
             $values = $request->input();
+			$values['category'] = json_decode($values['category'], TRUE);
+			$values['status'] 	= json_decode($values['status'], TRUE);
+			$values['tags'] 	= json_decode($values['tags'], TRUE);
 
             // VALIDACIONES DE CAMPOS OBLIGATORIOS POR LARAVEL
             $rules = [
-                'petName' => 'required',
-                'photo'   => 'required',
+                'petName'  => 'required',
+                'category' => 'required',
+                // 'photo'    => 'required|file',
             ];
             $input = [
-                'petName'   => $values['petName'],
-                'photo'     => $values['photo'],
+                'petName'  => $values['petName'],
+                'category' => $values['category'],
+                // 'photo'    => $values['photo'],
             ];
             $erros = [
-                'petName.required' => 'Nombre de mascota abligatorio',
-                'photo.required'   => 'Foto es abligatoria',
+                'petName.required'  => ' Nombre de mascota abligatorio',
+                'category.required' => ' Categoría es abligatorio',
+                // 'photo.required'    => ' Foto es abligatoria',
             ];
 
             $validate = Validator::make($input, $rules, $erros);
             if($validate->fails()){
                 $messages = $validate->messages()->all();
                 return ['error' => 1, 'info' => $messages];
+            }
+
+
+            ### Manejo datos foto enviada y validaccón ###
+            if( $_FILES ){
+                $tipoArchivo   = $_FILES['photo']['type'];
+                $nombreArchivo = $_FILES['photo']['name'];
+                $tamanoArchivo = $_FILES['photo']['size'];
+                $imagenCargada = fopen($_FILES['photo']['tmp_name'],'r');
+                $binarios      = fread($imagenCargada, $tamanoArchivo);
+                $extension = explode('.', $_FILES['photo']['name']);
+                $extension = end( $extension );
+
+                if ( $extension != 'jpg' && $extension != 'jpeg' && $extension != 'png' && $extension != 'gif' && $extension != 'bmp' ) {
+                    return ['error' => 1, 'info' => 'Formato de Archivo Incorrecto'];
+                }
             }
 
             // SI TRAE idPet MODIFICA SINO CREA NUEVO
@@ -81,9 +103,8 @@ class PetController extends Controller
                 $pet = PetModels::findOrFail($values['idPet']);
             }
             $pet->name 		= $values['petName'];
-            $pet->category  = $values['category']['id'];
-            $pet->photourls = $values['photo'];
-            $pet->status 	= $values['status']['id'];
+            $pet->category  = ($values['category']['id'] ? $values['category']['id'] : null );
+            $pet->status 	= (isset($values['status']['id']) ? $values['status']['id'] : null);
 
             if($pet->save()){
             	if($values['idPet']) {
@@ -99,6 +120,20 @@ class PetController extends Controller
                 		return ['error' => 1, 'info' => 'Error guardando etiqueta ' . $data['name']];
             		}
             	}
+
+            	if( $_FILES ){
+	            	## Si no existe directorio lo crea ##
+		    		if ( !file_exists("photos/") ) {
+		                mkdir("photos/", 0777, true);
+		            }
+		            ## Mueve archivo
+		            if ( !move_uploaded_file($_FILES["photo"]["tmp_name"], "photos/". $pet->id . "_" . date("His") . "." .$extension ) ) {
+		                return ['error' => 1, 'info' => 'Error copiando Imagen'];
+		            }
+		            $ruta = "/photos/". $pet->id . "_" . date("His") .".".$extension;
+		            PetModels::where('id', $pet['id'])->update(['photourls' => $ruta]);
+		        }
+
             } else {
                 DB::rollback();
                 return ['error' => 1, 'info' => 'No ha sido posible Guardar mascota.'];
@@ -126,8 +161,8 @@ class PetController extends Controller
             $TA 		= $tag->getTable();
 
             $pets = $pet->select("$PE.id", "$PE.name", "$PE.category", "$PE.photourls", "$PE.status", "$ST.name as statusName", "$CA.name as categoryName")
-            ->join("$ST", "$PE.status", "$ST.id")
             ->join("$CA", "$PE.category", "$CA.id")
+            ->leftJoin("$ST", "$PE.status", "$ST.id")
             ->where(function($query) use ($values, $PE) {
                 if(!empty($values['inputSearch'])) {
                     $query->orWhere("$PE.name", 'like', "%".$values['inputSearch']."%");
